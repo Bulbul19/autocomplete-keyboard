1568365c7b74225ad294a616707fd39d507d581c
Bulbul19 <bulbul.adeora@gmail.com>
2026-06-02 20:25:52 +0530

    Secure API credentials using environment variables

--- app/config.py
+++ app/config.py
@@ -2,6 +2,5 @@ import os
 from groq import Groq
 
 # Set your Groq API key here OR use environment variable GROQ_API_KEY
-API_KEY = os.environ.get("GROQ_API_KEY", "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")
-
-client = Groq(api_key=API_KEY)
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+client = Groq(api_key=GROQ_API_KEY)
